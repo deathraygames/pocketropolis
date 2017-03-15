aeeb726e5c1895be7b5f21ec1122f811a6baba3e
Luke Nickerson <luke.nickerson@gmail.com>
2017-03-15 16:09:57 -0400

    Added icons

--- scripts/pocketropolis-data.js
+++ scripts/pocketropolis-data.js
@@ -1,4 +1,18 @@
 data = {
+	"zoneTypes": {
+		"R": {
+			name: "Residential"
+		},
+		"C": {
+			name: "Commercial"
+		},
+		"I": {
+			name: "Industrial"
+		},
+		"P": {
+			name: "Public"
+		}
+	}
 	"resources": {
 		"milk": {
 			buy: 10
@@ -13,41 +27,45 @@ data = {
 	},
 	"buildings": {
 		"R-low": {
-			type: "R",
+			zoneType: "R",
 			maxFloors: 2
 		},
 		"R-high": {
-			type: "R",
+			zoneType: "R",
 			maxFloors: 7
 		},
 		"C-low": {
+			zoneType: "C",
 			maxFloors: 3
 		},
 		"C-high": {
+			zoneType: "C",
 			maxFloors: 7
 		},
 		"I-low": {
+			zoneType: "I",
 			maxFloors: 2
 		},
 		"I-high": {
+			zoneType: "I",
 			maxFloors: 5
 		}
 	},
 	"floors": {
 		"R-1": {
-			type: "R",
+			zoneType: "R",
 			imageFileName: "R-1.png",
 			buildings: ["R-low", "R-high"]
 		},
 		"C-1": {
-			type: "C",
+			zoneType: "C",
 			imageFileName: "C-1.png",
 			buildings: ["C-low", "C-high"]
 			jobs: 2,
 			noise: 10
 		},
 		"I-1": {
-			type: "I",
+			zoneType: "I",
 			imageFileName: "I-1.png",
 			buildings: ["I-low", "I-high"]
 			jobs: 3,
